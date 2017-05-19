9838d912df57248026a2ffb962cd59d7af264978
Halfwit <michaelmisch1985@gmail.com>
2017-05-19 09:46:29 -0600

    Adding check on setting atom
    Signed-off-by: Halfwit <michaelmisch1985@gmail.com>

--- grp.c
+++ grp.c
@@ -17,15 +17,28 @@ usage(char *name) {
 	exit(1);
 }
 
-static void 
-set_group(char *grp, xcb_window_t wid) {
+xcb_atom_t
+get_atom() {
 	xcb_intern_atom_cookie_t cookie;
 	xcb_intern_atom_reply_t *reply;
 	cookie = xcb_intern_atom(conn, 0, strlen("_WM_GROUP"), "_WM_GROUP");
 	if ((reply = xcb_intern_atom_reply(conn, cookie, NULL))) {
-		xcb_change_property_checked(conn, XCB_PROP_MODE_REPLACE, wid, reply->atom, XCB_ATOM_STRING, 8, strlen(grp), grp);
+		return reply->atom;
 		free(reply);
 	}
+	return 0;
+}
+
+void 
+set_group(char *grp, xcb_window_t wid) {
+		xcb_atom_t atom = get_atom();
+		xcb_void_cookie_t cookie;
+		cookie = xcb_change_property(conn, XCB_PROP_MODE_REPLACE, wid, atom, XCB_ATOM_STRING, 8, strlen(grp), grp);
+		xcb_generic_error_t *error;
+		if ((error = xcb_request_check(conn, cookie))) {
+			fprintf(stderr, "unable to set group\n");
+			free(error);
+		}
 }
 
 int
@@ -35,7 +48,6 @@ main(int argc, char *argv[]) {
 	}
 	init_xcb(&conn);
 	set_group(argv[1], strtoul(argv[2], NULL, 16 ));
-	xcb_flush(conn);
 	kill_xcb(&conn);
   	return 0;
 }
